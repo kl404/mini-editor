a31ad2ed93c441a6f2ea08585c74cd8f5ee6a324
kl404 <1466034796@qq.com>
2025-01-28 01:00:19 +0800

    feat: unit1

---
+++ src/editor/components/EditArea/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useComponetsStore } from "../../stores/components"
+
+export function EditArea() {
+
+    const {components, addComponent} = useComponetsStore();
+
+    useEffect(()=> {
+        addComponent({
+            id: 222,
+            name: 'Container',
+            props: {},
+            children: []
+        }, 1);
+        addComponent({
+            id: 333,
+            name: 'Video',
+            props: {},
+            children: []
+        }, 222);
+        
+    }, []);
+
+    return <div>
+        <pre>
+            {
+                JSON.stringify(components, null, 2)
+            }
+        </pre>
+    </div>
+}
