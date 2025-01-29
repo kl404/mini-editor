a31916a004a2ce874eaabe8c86e11065eb578f2c
kl404 <1466034796@qq.com>
2025-01-29 19:17:18 +0800

    feat: unit2

--- src/editor/components/Material/index.tsx
+++ src/editor/components/Material/index.tsx
@@ -1,3 +1,18 @@
-export  function Material() {
-  return <div>Material</div>;
+import { useMemo } from "react";
+import { useComponentConfigStore } from "../../stores/component-config";
+import { MaterialItem } from "../MaterailItem";
+
+export function Material() {
+    const { componentConfig } = useComponentConfigStore();
+
+    const components = useMemo(() => {
+        return Object.values(componentConfig);
+     }, [componentConfig]);
+
+    return <div>{
+      components.map((item, index) => {
+        return <MaterialItem name={item.name} key={item.name + index}/>
+    })
+    
+    }</div>
 }
--- src/editor/components/Setting/index.tsx
+++ src/editor/components/Setting/index.tsx
@@ -1,3 +1,11 @@
-export  function Setting() {
-  return <div>Setting</div>;
+import { useComponetsStore } from "../../stores/components";
+
+export function Setting() {
+    const { components } = useComponetsStore();
+
+    return <div>
+        <pre>
+            {JSON.stringify(components, null, 2)}
+        </pre> 
+    </div>
 }
